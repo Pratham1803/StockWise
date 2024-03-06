c654d0c62093cf4fef1978c617fec5073fd9dde4
pratham <prathamrathod1812@gmail.com>
2024-03-06 17:11:53 +0530

    Person Recycler view set for all three types with changing random images.

--- app/src/main/java/com/example/stockwise/fragments/person/PersonFragment.java
+++ app/src/main/java/com/example/stockwise/fragments/person/PersonFragment.java
@@ -1,16 +1,19 @@
 package com.example.stockwise.fragments.person;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,23 +24,28 @@ import com.example.stockwise.DialogBuilder;
 import com.example.stockwise.Params;
 import com.example.stockwise.R;
 import com.example.stockwise.databinding.FragmentPersonBinding;
-import com.example.stockwise.databinding.FragmentProductBinding;
-import com.example.stockwise.fragments.category.ManageCategory;
-import com.example.stockwise.model.CategoryModel;
 import com.example.stockwise.model.PersonModel;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.hbb20.CountryCodePicker;
+
+import java.util.ArrayList;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
-public class PersonFragment extends Fragment {
+public class PersonFragment extends Fragment implements AdapterView.OnItemSelectedListener{
     private Context context; // to store context
     private FragmentPersonBinding bind; // bind view
     String[] ContactFilter = {"All Contacts", "Customers", "Vendors"};
     private AlertDialog alertDialog; // to store alert dialog
     private SweetAlertDialog sweetAlertDialog; // to store sweet alert dialog
+    private PersonAdapter personAdapter; // to store person adapter
+    private ArrayList<PersonModel> arrAllPerson; // to store person list
+    private ArrayList<PersonModel> arrAllVendors; // to store Vendors list
+    private ArrayList<PersonModel> arrAllCustomers; // to store Customers list
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,6 +64,7 @@ public class PersonFragment extends Fragment {
         // Set the ArrayAdapter (ad) data on the
         // Spinner which binds data to spinner
         bind.FilterContacts.setAdapter(ad);
+        bind.FilterContacts.setOnItemSelectedListener(this);
 
         // click on add contact button
         bind.btnAddContact.setOnClickListener(new View.OnClickListener() {
@@ -65,8 +74,55 @@ public class PersonFragment extends Fragment {
             }
         });
 
+        // set recycler view
+        arrAllPerson = new ArrayList<PersonModel>(); // initializing Array list of personModule
+        arrAllVendors = new ArrayList<PersonModel>(); // initializing Array list of personModule
+        arrAllCustomers = new ArrayList<PersonModel>(); // initializing Array list of personModule
+        personAdapter = new PersonAdapter(arrAllPerson,context); // initializing personAdapter
+        bind.recyclerContacts.setLayoutManager(new LinearLayoutManager(context)); // setting layout manager of recycler view
+        bind.recyclerContacts.setAdapter(personAdapter); // setting adapter to the recycler view
+
+        // collecting all contacts from firebase
+        dbGetAllContacts();
+
         // Inflate the layout for this fragment
         return bind.getRoot();
+    }
+
+    // get all contacts from firebase and set in array list
+    private void dbGetAllContacts() {
+        DatabaseReference parentRef =  Params.getREFERENCE().child(Params.getPERSON());
+
+        // collecting all customers from firebase
+        parentRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrAllCustomers.clear();
+                arrAllVendors.clear();
+                arrAllPerson.clear();
+                if (snapshot.child(Params.getCUSTOMER()).exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.child(Params.getCUSTOMER()).getChildren()) {
+                        PersonModel personModel = dataSnapshot.getValue(PersonModel.class);
+                        arrAllCustomers.add(personModel);
+                    }
+                }
+                if (snapshot.child(Params.getVENDOR()).exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.child(Params.getVENDOR()).getChildren()) {
+                        PersonModel personModel = dataSnapshot.getValue(PersonModel.class);
+                        arrAllVendors.add(personModel);
+                    }
+                }
+                arrAllPerson.addAll(arrAllCustomers);
+                arrAllPerson.addAll(arrAllVendors);
+                personAdapter.setLocalDataset(arrAllPerson);
+                personAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("ErrorMsg", "onCancelled: "+error.getMessage());
+            }
+        });
     }
 
     // add person button click
@@ -209,5 +265,24 @@ public class PersonFragment extends Fragment {
                 alertDialog.dismiss();
             }
         });
+    }
+
+    // Filter of contacts select listener
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String currentSelection = ContactFilter[position];
+        if(currentSelection.equals(ContactFilter[0])){
+            personAdapter.setLocalDataset(arrAllPerson);
+        }else if(currentSelection.equals(ContactFilter[1])){
+            personAdapter.setLocalDataset(arrAllCustomers);
+        }else if (currentSelection.equals(ContactFilter[2])){
+            personAdapter.setLocalDataset(arrAllVendors);
+        }
+        personAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
