b1947c0728003e60c5aad667f040bdd174886a17
pratham <prathamrathod1812@gmail.com>
2024-03-09 15:43:58 +0530

    minor Design changes in profile, and setting layout

--- app/src/main/java/com/example/stockwise/fragments/product/ProductFragment.java
+++ app/src/main/java/com/example/stockwise/fragments/product/ProductFragment.java
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
